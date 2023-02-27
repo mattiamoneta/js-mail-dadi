1cf3d7348254680c1bd56fdb3e986f323f8520ee
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-27 16:20:10 +0100

    Final (Added HTML+CSS)

--- js-dadi/js/main.js
+++ js-dadi/js/main.js
@@ -1,13 +1,32 @@
-const randSys = Math.floor(Math.random() * 6) + 1;
-const randUsr = Math.floor(Math.random() * 6) + 1;
+const DomUsrNumber = document.getElementById('usrNumber');
+const DomSysNumber = document.getElementById('sysNumber');
+const btnSend = document.getElementById('btnSend');
+const msg = document.getElementById('msg');
 
-console.log(`Numero del computer: ${randSys}`);
-console.log(`Il tuo numero: ${randUsr}`);
 
-if (randSys > randUsr){
-    console.log("Vince il computer!");
-} else if(randUsr > randSys) {
-    console.log("Hai vinto!");
-} else {
-    console.log("Parità!");
-}
+btnSend.addEventListener('click', function(){
+
+    const randSys = Math.floor(Math.random() * 6) + 1;
+    const randUsr = Math.floor(Math.random() * 6) + 1;
+
+    console.log(`Numero del computer: ${randSys}`);
+    DomSysNumber.innerHTML = randSys;
+    console.log(`Il tuo numero: ${randUsr}`);
+    DomUsrNumber.innerHTML = randUsr;
+
+    if (randSys > randUsr){
+        console.log("Vince il computer!");
+        msg.innerHTML = "Vince il computer!";
+        
+    } else if(randUsr > randSys) {
+        console.log("Hai vinto!");
+        msg.innerHTML = "Hai Vinto!";
+        
+    } else {
+        console.log("Parità!");
+        msg.innerHTML = "Parità!";
+        
+    }
+
+});
+

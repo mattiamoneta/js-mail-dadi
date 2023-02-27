ab36962931f45a4351d0ea33cf2f2d48ef5b6803
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-27 15:47:30 +0100

    Added HTML+CSS

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -1,18 +1,37 @@
-const inputMail = prompt("Inserire email da validare: ");
+let DomTxtMail = document.getElementById('txtEmail');
+let DomResp = document.getElementById('resp');
+const btnSend = document.getElementById('btnSend')
+
 const dbMail = ["mail1@example.com","mail2@example.com","mail3@example.com","mail4@example.com","mail5@example.com"];
 
 
-if (inputMail != ""){
+btnSend.addEventListener('click', function(){
+    const inputMail = DomTxtMail.value;
 
-    for (let i = 0; i < dbMail.length; i++){
+    if (inputMail != ""){
 
-        if (inputMail == dbMail[i]){
-            console.log("Mail Trovata!");
-            break;
-        } else if ((i == dbMail.length - 1) && (inputMail != dbMail[i])){
-            console.log("Nessuna mail trovata");
+        for (let i = 0; i < dbMail.length; i++){
+    
+            if (inputMail == dbMail[i]){
+                console.log("Mail Trovata!");
+                DomResp.innerHTML = "Mail Trovata!";
+                DomResp.classList.add('dialog-success','visible');
+                break;
+            } else if ((i == dbMail.length - 1) && (inputMail != dbMail[i])){
+                console.log("Nessuna mail trovata");
+                DomResp.innerHTML = "Nessuna mail trovata";
+                DomResp.classList.remove('dialog-success');
+                DomResp.classList.add('dialog-error','visible');
+            }
+    
         }
-
+    
+    } else {
+                console.log("Il campo mail non può essere vuoto");
+                DomResp.innerHTML = "Il campo mail non può essere vuoto";
+                DomResp.classList.remove('dialog-success');
+                DomResp.classList.add('dialog-error','visible');
     }
+});
 
-} 
+

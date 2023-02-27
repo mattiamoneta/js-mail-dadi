2fdea3559af5e9e0734520eba02559da83fbcc2f
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-27 16:37:18 +0100

    Rimossa istruzione Break

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -8,29 +8,35 @@ const dbMail = ["mail1@example.com","mail2@example.com","mail3@example.com","mai
 btnSend.addEventListener('click', function(){
     const inputMail = DomTxtMail.value;
 
+    let mailFound = false;
+
     if (inputMail != ""){
 
         for (let i = 0; i < dbMail.length; i++){
     
             if (inputMail == dbMail[i]){
-                console.log("Mail Trovata!");
-                DomResp.innerHTML = "Mail Trovata!";
-                DomResp.classList.add('dialog-success','visible');
-                break;
-            } else if ((i == dbMail.length - 1) && (inputMail != dbMail[i])){
-                console.log("Nessuna mail trovata");
-                DomResp.innerHTML = "Nessuna mail trovata";
-                DomResp.classList.remove('dialog-success');
-                DomResp.classList.add('dialog-error','visible');
-            }
+                mailFound = true; 
+            } 
     
         }
+
+        if (mailFound == true){
+            console.log("Mail Trovata!");
+            DomResp.innerHTML = "Mail Trovata!";
+            DomResp.classList.add('dialog-success','visible');
+        } else {
+            console.log("Nessuna mail trovata");
+            DomResp.innerHTML = "Nessuna mail trovata";
+            DomResp.classList.remove('dialog-success');
+            DomResp.classList.add('dialog-error','visible');
+        }
+
     
     } else {
-                console.log("Il campo mail non può essere vuoto");
-                DomResp.innerHTML = "Il campo mail non può essere vuoto";
-                DomResp.classList.remove('dialog-success');
-                DomResp.classList.add('dialog-error','visible');
+            console.log("Il campo mail non può essere vuoto");
+            DomResp.innerHTML = "Il campo mail non può essere vuoto";
+            DomResp.classList.remove('dialog-success');
+            DomResp.classList.add('dialog-error','visible');
     }
 });
 
